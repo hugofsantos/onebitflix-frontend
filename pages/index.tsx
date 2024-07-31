05b6c6964c8416019165cc151064441e0bdd155a
Hugo Santos <hf.santos2332@gmail.com>
2024-07-31 16:46:16 -0300

    Adiciona estrutura de slides no HomeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, ReactNode } from "react";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
 import CardsSection from "@/src/components/homeNoAuth/cardsSection";
 import styles from "../styles/HomeNoAuth.module.scss";
+import SlideSection from "@/src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "@/src/services/courseService";
 
-const HomeNoAuth = () => {
+interface IndexPageProps {
+  children?: ReactNode;
+  courses: Array<CourseType> 
+}
+
+const HomeNoAuth = ({courses}: IndexPageProps) => {
   return (
     <Fragment>
       <Head>
@@ -19,12 +27,22 @@ const HomeNoAuth = () => {
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection>
-
-        </CardsSection>
+        <CardsSection/>
+        <SlideSection newestCourses={courses}/>
       </main>
     </Fragment>
   );
 };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses();
+
+  return {
+    props: {
+      courses: (res as any).data
+    },
+    revalidate: 60 * 60 * 24 // Um dia (24h) em segundos
+  }
+}
 
 export default HomeNoAuth;
---
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -0,0 +1,23 @@
+import { CourseType } from '@/src/services/courseService';
+import {Container, Button} from 'reactstrap';
+import styles from './styles.module.scss';
+import SlideComponent from '../../common/slideComponent';
+import Link from 'next/link';
+
+interface props {
+  newestCourses: Array<CourseType>
+}
+
+const SlideSection = ({newestCourses} : props) => {
+  return <>
+    <Container>
+      <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
+      <SlideComponent courses={newestCourses}></SlideComponent>
+      <Link href="/register">
+        <Button outline color="light" className={styles.slideSectionBtn}>Se cadastre para acessar!</Button>
+      </Link>
+    </Container>
+  </>;
+}
+
+export default SlideSection;
