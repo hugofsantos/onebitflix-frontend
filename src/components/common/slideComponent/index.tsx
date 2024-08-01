74887bc12a7a5c59752d45c805f8fed722d079e2
Hugo Santos <hf.santos2332@gmail.com>
2024-08-01 15:57:41 -0300

    Melhora responsividade dos slides do homeNoAuth

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -8,14 +8,35 @@ interface props {
 };
 
 const SlideComponent = ({courses}: props) => {
+  const slideCount = courses.length > 4 ? 4 : courses.length;
+
   return <>
     <div className='d-flex flex-column align-items-center py-4'>
       <Splide options={{
         type: "loop",
-        perPage: 4,
+        perPage: slideCount,
         perMove: 1,
-        width: 1200,
-        pagination: false
+        width: slideCount * 300,
+        pagination: false,
+        arrows: courses.length > 4,
+        drag: courses.length > 4,
+        breakpoints: {
+          1200: {
+            perPage: slideCount >= 2 ? 2 : 1,
+            width: slideCount >= 2 ? 600: 300,
+            arrows: courses.length > 2,
+            drag: courses.length > 2,            
+          },
+          600: {
+            perPage: 1,
+            width: 300,
+            arrows: courses.length > 1,
+            drag: courses.length > 1,            
+          },
+          300: {
+            width: 250
+          }
+        }
       }}>
         {
           courses?.map((course) => (
